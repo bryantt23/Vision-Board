25248aece8cab72a667dbd4a7986f98b61be7259
bryant tee <bryantt23@gmail.com>
2014-12-30 02:54:37 -0800

    b

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 
-
-//     i think i got this to work
-    // $('img').hide();
-    
-    // $('img').animate({ top: '+=100px' }, 1000 );
-
-    // $('img').click(function() {
-        // $(this).effect('bounce', {times:3}, 500);
-    // });
-    
+    $('div').click(function() {
+        $(this).effect('slide');
     });
+});
