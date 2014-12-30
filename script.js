45674fb620e50c37d8b8e83a17f285534f62f35f
bryant tee <bryantt23@gmail.com>
2014-12-30 02:24:10 -0800

    hide

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+
+//     i think i got this to work
+    $('img').hide();
+    
+    // $('img').animate({ top: '+=100px' }, 1000 );
+
+    // $('img').click(function() {
+        // $(this).effect('bounce', {times:3}, 500);
+    // });
+    
+    });
